6a9e41ea053a05d36d9a6c4d8bbf39b800d78684
Jovica Jovanovic <jovica.jovanovic@europos.co.rs>
2018-05-17 15:07:38 +0200

    added accordion like related lists to workorder-details page

--- src/pages/workorder-details/workorder-details.ts
+++ src/pages/workorder-details/workorder-details.ts
@@ -12,9 +12,7 @@ export class WorkorderDetailsPage {
 
   tab: string = "details";
   currWO: any;
-  woParts: any;
-  woExpenses: any;
-  woLabors: any;
+  relatedData: Array<any> = [];
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private oauth : OAuthServiceProvider, private woService: WorkordersServiceProvider) {
   }
@@ -28,12 +26,59 @@ export class WorkorderDetailsPage {
       .then(oauth => {
         this.woService.getWODetails(oauth, woID)
           .then(result => {
-            console.log("result = ", result);
             this.currWO = result.currWO;
-            this.woParts = result.woParts;
-            this.woExpenses = result.woExpenses;
-            this.woLabors = result.woLabors;
+
+            // so ugly because I wanted to use existing functions to get required data
+            // and data structures are different, so I needed to do this
+            let woParts = result.woParts.map(woPart => {
+              let obj = {};
+              obj["relatedName"] = woPart.Name;
+              obj["cost"] = woPart.UH__Cost__c;
+              obj["createdDate"] = new Date(woPart.CreatedDate).toDateString();
+              obj["quantity"] = woPart.UH__Quantity__c;
+              obj["totalCost"] = woPart.UH__totalCost__c;
+              obj["type"] = woPart.UH__Part__r.UH__Type__c;
+              // obj["typeName"] = woPart.UH__Part__r.Name;
+              obj["relatedObjectURL"] = woPart.attributes.url;
+              return obj;
+            });
+            let woExpenses = result.woExpenses.map(woExpense => {
+              let obj = {};
+              obj["relatedName"] = woExpense.Name;
+              obj["cost"] = woExpense.UH__Cost__c;
+              obj["createdDate"] = new Date(woExpense.CreatedDate).toDateString();
+              obj["quantity"] = woExpense.UH__Quantity__c;
+              obj["totalCost"] = woExpense.UH__totalCost__c;
+              obj["type"] = woExpense.UH__expenseType__c;
+              // obj["typeName"] = "";
+              obj["relatedObjectURL"] = woExpense.attributes.url;
+              return obj;
+            });
+            let woLabors = result.woLabors.map(woLabor => {
+              let obj = {};
+              obj["relatedName"] = woLabor.Name;
+              obj["cost"] = woLabor.UH__Cost__c;
+              obj["createdDate"] = new Date(woLabor.CreatedDate).toDateString();
+              obj["quantity"] = woLabor.UH__hoursCount__c;
+              obj["totalCost"] = woLabor.UH__totalCost__c;
+              obj["type"] = woLabor.UH__Labor__r.UH__Type__c;
+              // obj["typeName"] = woLabor.UH__Labor__r.Name;
+              obj["relatedObjectURL"] = woLabor.attributes.url;
+              return obj;
+            });
+
+            this.relatedData.push({"name": "WO Parts", "elements": woParts, "size": woParts.length});
+            this.relatedData.push({"name": "WO Expenses", "elements": woExpenses, "size": woExpenses.length});
+            this.relatedData.push({"name": "WO Labors", "elements": woLabors, "size": woLabors.length});
           });
       });
+  }
+
+  toggleSection(i) {
+    this.relatedData.forEach((elem, idx) => {
+      // close all opened sections that are not clicked
+      if (idx !== i && elem.open) elem.open = !elem.open; 
+    });
+    this.relatedData[i].open = !this.relatedData[i].open;
   }
 }
