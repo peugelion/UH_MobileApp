ca971f198a93e2e96d418947fc83b61fa6270c92
Milan Petrović <millllan@gmail.com>
2018-10-02 17:16:26 +0200

    PWA + strapi fix

--- src/providers/constants/constants.ts
+++ src/providers/constants/constants.ts
@@ -1,7 +1,9 @@
 
 //export const STRAPI_ENDPOINT = 'http://localhost:1337';
 //export const STRAPI_ENDPOINT = window.location.protocol + '//' + window.location.hostname;
-export const STRAPI_ENDPOINT = (window.location.hostname == "localhost") ? 'http://localhost:1337' : window.location.origin; // public url works on port 80
+//export const STRAPI_ENDPOINT = (window.location.hostname == "localhost") ? 'http://localhost:1337' : window.location.origin; // public url works on port 80
+export const STRAPI_ENDPOINT = (window.location.hostname == "localhost") ? 'http://localhost:1337' : 'https://uhstrapi.app.sajtweb.net'; // public url works on port 80 .. hardcoded for now
+
 export const DATE_TIME_FMT= `short`;
 export const DATE_FMT= `date`;
 // export class Constants {
